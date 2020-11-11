4a6bfa623ff9208c3ede6cbefcec28aaada0af6e
eunjin0212 <eunjin9639@gmail.com>
2020-11-11 18:13:46 +0900

    TVContainer

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,3 +1,4 @@
+import { tvApi } from "api";
 import React from "react";
 import TVPresenter from "./TVPresenter";
 // eslint-disable-next-line
@@ -9,6 +10,30 @@ export default class extends React.Component {
     loading: true,
     error: null,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      this.setState({ topRated, popular, airingToday });
+    } catch {
+      this.setState({
+        error: "Can't find TV Show information.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
   render() {
     const { topRated, popular, airingToday, loading, error } = this.state;
     return (
