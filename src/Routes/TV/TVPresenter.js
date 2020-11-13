4a7a704629b0db1699bce47382c4360e0461933e
eunjin0212 <eunjin9639@gmail.com>
2020-11-13 14:55:12 +0900

    Poster

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
-import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
   padding: 10px 20px;
@@ -15,17 +15,44 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
     <Container>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
-          {topRated.map((show) => show.name)}
+          {topRated.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+            />
+          ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Shows">
-          {popular.map((show) => show.name)}
+          {popular.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+            />
+          ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today">
-          {airingToday.map((show) => show.name)}
+          {airingToday.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+            />
+          ))}
         </Section>
       )}
     </Container>
